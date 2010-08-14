c214ead3f8e881a4a1c46eb94513518280d47eaa
Alexandr Ciornii <alexchorny@gmail.com>
2010-08-14 19:15:02 +0300

    code cleaning

--- lib/WWW/librusec.pm
+++ lib/WWW/librusec.pm
@@ -15,17 +15,17 @@ sub scrape_news {
         =>
         scraper {
           #print $d "scraper by date\n";
-          process '//h4', 'date'=>'TEXT';
+          process 'h4', 'date'=>'TEXT';
           split_by 'p.genre', 'genre_list[]' => scraper {
-             my $tree = $_[0];
+             #my $tree = $_[0];
              #print $d " split by genre\n";
              #print $d " working on ",$tree->starttag,"\n";
              #process q{a.genre}, "genres[]" => {title => 'TEXT'};
              process q{a.genre}, "genres[]" => 'TEXT';
 
              split_by 'div#z0', 'book_list[]' => scraper {
-               my $tree = $_[0];
-               $tree->dump($d);
+               #my $tree = $_[0];
+               #$tree->dump($d);
                #print $d "  split by book\n";
                #print $d "  working on ",$tree->starttag,"\n";
                process 'a', 'book_links[]' => { r => '@href', title => 'TEXT' };
